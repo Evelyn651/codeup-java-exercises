439929c48c9dd40953fe10c6606d9d51266034ba
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-05-01 18:24:40 -0500

    feat: completed console input output exercises

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class ConsoleExercises {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double pi = 3.14159;
+        System.out.printf("The value of pi is approximately %.2f%n", pi);
+
+        System.out.println("Give me a number:");
+        int userNumber = Integer.parseInt(input.nextLine());
+        System.out.println("userNumber = " + userNumber);
+
+        String param1 = input.nextLine();
+        String param2 = input.nextLine();
+        String param3 = input.nextLine();
+
+        System.out.println("param1 = " + param1);
+        System.out.println("param2 = " + param2);
+        System.out.println("param3 = " + param3);
+
+        System.out.println("How are you feeling?");
+        String sentence = input.nextLine();
+        System.out.println("sentence = " + sentence);
+
+        System.out.println("Give me the width:");
+        float width = Float.parseFloat(input.nextLine());
+        System.out.println("Give me the length:");
+        float length = Float.parseFloat(input.nextLine());
+
+        float area = width * length;
+        float perimeter = (width * 2) + (length * 2);
+
+        System.out.println("perimeter = " + perimeter);
+        System.out.println("area = " + area);
+
+
+    }
+}
