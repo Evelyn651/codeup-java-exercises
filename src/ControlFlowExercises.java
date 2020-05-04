594561a8df252cdd5178ffedc058a1d2a9bfd0c8
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-05-04 15:04:44 -0500

    feat: exercises before walk through

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,10 @@
+public class ControlFlowExercises {
+    public static void main (String[] args){
+        System.out.println("Control Flow Exercises");
+        int i = 5;
+        while(i <= 15){
+            ++i;
+            System.out.println(i);
+        }
+    }
+}
