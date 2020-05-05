fd9841c26dfe5730647d37dfeccc209c69e1868f
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-05-05 09:08:50 -0500

    feat: new file and branch for Strings lecture notes and exercise

---
+++ src/StringsLec.java
@@ -0,0 +1,9 @@
+public class StringsLec {
+    public static void main(String[] args){
+        String message = "Hello";
+        System.out.println(message);
+
+        message = message + "World!";
+        System.out.println(message);
+    }
+}
