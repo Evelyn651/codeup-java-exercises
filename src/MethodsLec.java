f533807a00505e9546c5cfc06c4310fff2532846
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-05-05 12:24:26 -0500

    feat: saw similarities and diiferences between functions in js and methods in java.

--- src/MethodsLec.java
+++ src/MethodsLec.java
@@ -1,14 +1,21 @@
 public class MethodsLec {
     public static void main(String[] args){
-        System.out.println(sayHello());
+        System.out.println(sum(2,4));
+        System.out.println(sayHello("kili"));
 
     }
 
-    // function sayHello(){
+    // function sayHello(param1, param2){
     //     return "Hi Fer";
     // }
     // sayHello(); //call back the function in js. cant do this in java.
-    public static String sayHello (){
-        return "Hi Fer";
+    public static String sayHello (String name){
+        sum(2,4);
+        return "Hi " + name;//data type^ and ^ parameter
+    }
+
+    public static int sum(int num1, int num2){ //need to specify the data type for each parameter
+        return num1 + num2;
+
     }
 }
