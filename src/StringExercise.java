ec8ece016333ddb2a9e0c84f1104c348fd9ab66d
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-05-05 11:54:41 -0500

    feat: incomplete exercise for strings

---
+++ src/StringExercise.java
@@ -0,0 +1,9 @@
+public class StringExercise {
+    public static void main(String[] args){
+        String education = "We don't need no education";
+        System.out.println(education);
+        String thought = "We don't need no thought control";
+
+    }
+}
+
