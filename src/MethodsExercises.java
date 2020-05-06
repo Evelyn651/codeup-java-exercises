368ef28e99c5f9f89b40f71f86c071c614201ba5
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-05-05 20:49:11 -0500

    feat: completed method for addition under basic arithmetic

---
+++ src/MethodsExercises.java
@@ -0,0 +1,10 @@
+public class MethodsExercises {
+    public static void main(String[] args){
+        System.out.println(addition(5,3));
+    }
+    //addition
+    public static int addition(int num1, int num2){
+        return num1 + num2;
+    }
+
+}
