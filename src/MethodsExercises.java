47a004be3584c8326f70378bdc83e94545f6efa3
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-05-05 20:53:28 -0500

    feat: completed subtraction method under basic arithmetic

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,10 +1,15 @@
 public class MethodsExercises {
     public static void main(String[] args){
         System.out.println(addition(5,3));
+        System.out.println(subtraction(6, 2));
+        System.out.println(multiplication(4, 3));
     }
     //addition
     public static int addition(int num1, int num2){
         return num1 + num2;
     }
+    //subtraction
+    public static int subtraction(int num3, int num4){
+        return num3 - num4;
 
 }
