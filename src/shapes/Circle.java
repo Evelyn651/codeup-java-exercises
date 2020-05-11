ccbe4469b1684fcd4b709724cce432f4bc8510cd
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-05-10 20:46:12 -0500

    feat: exercise 4 is finished with the bonus.

---
+++ src/shapes/Circle.java
@@ -0,0 +1,23 @@
+package shapes;
+
+public class Circle {
+    private double radius;
+    static private int count = 0;
+
+    public Circle(double aRadius){
+        this.radius = aRadius;
+        count++;
+    }
+
+    public static int getCount(){
+        return count;
+    }
+
+    public double getArea(){
+        return Math.PI * (Math.pow(this.radius, 2));
+    }
+
+    public double getCircumference(){
+        return 2 * Math.PI * this.radius;
+    }
+}
