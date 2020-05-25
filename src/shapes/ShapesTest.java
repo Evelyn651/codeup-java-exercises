08a81a5b0273654657c1cba9736c46a960733536
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-05-25 10:11:02 -0500

    feat: added ShapesTest exercise

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -1,5 +1,14 @@
 package shapes;
 
 public class ShapesTest {
+    public static void main(String[] args){
+        Meausureable myShape = new Square(5);
+        System.out.println("myShape. getArea() = " + myShape.getArea());
+        System.out.println("myShape.getPerimeter() = " + myShape.getPerimeter());
+
+        myShape = new Rectangle(2,3);
+        System.out.println("myShape.getArea() = " + myShape.getArea());
+        System.out.println("myShape.getPerimeter() = " + myShape.getPerimeter());
+    }
 
 }
