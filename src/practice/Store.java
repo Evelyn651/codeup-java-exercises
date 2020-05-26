9bb403f41438295c120e12b0e089fc4e25aacaf8
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-05-26 18:33:15 -0500

    added a new instance of cookieShop and printed out product and added new parameters and instance fields

--- src/practice/Store.java
+++ src/practice/Store.java
@@ -2,25 +2,35 @@ package practice;
 
 public class Store {
 
-        // declare instance fields here!
-        String productType;
+    // declare instance fields here!
+    String productType;
+    //new fields- can have multiple
+    int inventoryCount;
+    double inventoryPrice;
 
-        // constructor method
-        public Store(String product) {
-            System.out.println("I am inside the constructor method.");
-            productType = product;
-        }
+    // constructor method
+    public Store(String product, int count, double price) {
+        System.out.println("I am inside the constructor method.");
+        productType = product;
+        inventoryCount = count;
+        inventoryPrice = price;
+    }
 
-        // main method
-        public static void main(String[] args) {
-            System.out.println("This code is inside the main method.");
-            //created an instance of Store and assigned the variable lemonadeStand and used lemonade as the parameter
-            // value
-            Store lemonadeStand = new Store("lemonade");
+    // main method
+    public static void main(String[] args) {
+        System.out.println("This code is inside the main method.");
+        //created an instance of Store and assigned the variable lemonadeStand and used lemonade as the parameter
+        // value
+        Store lemonadeStand = new Store("lemonade", 6, 2.20);
+        Store cookieShop = new Store("cookies", 12, 3.75);
 
-            System.out.println(lemonadeStand.productType);
-        }
+        //Ordering matters. we must pass values into the constructor invocation in the same order that they're
+        // listed in the parameters
 
+        System.out.println("lemonadeStand.productType= " + lemonadeStand.productType +" "+ lemonadeStand.inventoryPrice);
+        System.out.println("cookieShop.productType= " + cookieShop.productType +" "+ cookieShop.inventoryPrice);
+
+    }
 
 
 }
