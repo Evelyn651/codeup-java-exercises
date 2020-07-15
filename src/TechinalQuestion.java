9dc6328bcf522ea1b343fe6c0fdc77d08abb4e27
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-07-15 00:59:23 -0500

    feat: java if/else statement practice

---
+++ src/TechinalQuestion.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+//Write a function to return a double value for the average of all numbers in a passed down int array.
+
+//public class TechinalQuestion {
+//     public double average(int values[]){
+//
+//     }
+//}
