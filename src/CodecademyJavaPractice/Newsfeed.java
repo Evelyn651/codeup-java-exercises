d6ae4807e5205c17b1eed2e92cb5106bf5208889
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-08-05 00:10:27 -0500

    feat: learning how to get the length of the array and print out.

--- src/CodecademyJavaPractice/Newsfeed.java
+++ src/CodecademyJavaPractice/Newsfeed.java
@@ -4,9 +4,10 @@ import java.util.Arrays;
 public class Newsfeed {
     String[] topics = {"Opinion", "Tech", "Science", "Health"};
     int[] views = {0,0,0,0};
+    String[] favoriteArticles;
 
     public Newsfeed(){
-
+        favoriteArticles = new String[10];
     }
 
     public String[] getTopics(){
@@ -17,8 +18,16 @@ public class Newsfeed {
         return topics[0];
     }
 
+    public int getNumTopics(){
+        return topics.length;
+    }
+
     public void viewTopic(int topicIndex){
         views[topicIndex] = views[topicIndex] + 1;
+    }
+
+    public void setFavoriteArticle(int favoriteIndex, String newArticle){
+        favoriteArticles[favoriteIndex] = newArticle;
     }
 
     public static void main(String[] args){
@@ -29,6 +38,7 @@ public class Newsfeed {
 
         System.out.println("The top topic is: " + sampleFeed.getTopTopic());
 
+        //views
         sampleFeed.viewTopic(1);
         sampleFeed.viewTopic(1);
         sampleFeed.viewTopic(3);
@@ -37,5 +47,15 @@ public class Newsfeed {
         sampleFeed.viewTopic(1);
 
         System.out.println("The " + sampleFeed.topics[1] + "topic has been viewed " + sampleFeed.views[1] + " times!");
+
+        //favorite articles
+        sampleFeed.setFavoriteArticle(2, "Humans: Exterminate Or Not?");
+        sampleFeed.setFavoriteArticle(3, "Organic Eye Implants");
+        sampleFeed.setFavoriteArticle(0, "Oil News");
+
+        System.out.println(Arrays.toString(sampleFeed.favoriteArticles));
+
+        //get the length of the array
+        System.out.println("The number of topics is " + sampleFeed.getNumTopics());
     }
 }
