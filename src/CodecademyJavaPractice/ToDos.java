9b2f726607e274a358efcc58194e1aa554586566
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-08-11 17:11:22 -0500

    feat: practicing arrays and arrayslist initializing.

---
+++ src/CodecademyJavaPractice/ToDos.java
@@ -0,0 +1,10 @@
+package CodecademyJavaPractice;
+import java.util.ArrayList;
+public class ToDos {
+    public static void main(String[] args){
+        //create a to do list below
+        ArrayList<String> toDoList = new ArrayList<>();
+
+
+    }
+}
