640a7afd51372986a432ca7bd7123106c99f94b1
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-08-11 17:20:29 -0500

    feat: creating things to add to arraylist and adding them with add().

--- src/CodecademyJavaPractice/ToDos.java
+++ src/CodecademyJavaPractice/ToDos.java
@@ -5,6 +5,17 @@ public class ToDos {
         //create a to do list below
         ArrayList<String> toDoList = new ArrayList<>();
 
+        //the todos to add to the arraylist
+        String toDo1 = "water plants";
+        String toDo2 = "practice coding";
+        String toDo3 = "read a book";
+
+        //adding the todos to the arraylist
+        toDoList.add(toDo1);
+        toDoList.add(toDo2);
+        toDoList.add(toDo3);
+
+        System.out.println("Your to-do list: " + toDoList);
 
     }
 }
