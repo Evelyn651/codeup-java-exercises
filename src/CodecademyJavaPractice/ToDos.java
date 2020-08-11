f251905c21a5c3c00c3406894bc83f76c7ec3f94
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-08-11 17:39:16 -0500

    feat: creating more arraylist and adding more things to it. Using size() to get the length of each arraylist.

--- src/CodecademyJavaPractice/ToDos.java
+++ src/CodecademyJavaPractice/ToDos.java
@@ -17,5 +17,31 @@ public class ToDos {
 
         System.out.println("Your to-do list: " + toDoList);
 
+        //Sherlock Holmes' to-do list
+        ArrayList<String> sherlocksToDos = new ArrayList<String>();
+
+        sherlocksToDos.add("visit the crime scene");
+        sherlocksToDos.add("play violin");
+        sherlocksToDos.add("interview suspects");
+        sherlocksToDos.add("solve the case");
+        sherlocksToDos.add("apprehend the criminal");
+
+        //Piorot's to-do list
+        ArrayList<String> poirotsToDos = new ArrayList<>();
+
+        poirotsToDos.add("visit the crime scene");
+        poirotsToDos.add("interview suspects");
+        poirotsToDos.add("let the little grey cells do their work");
+        poirotsToDos.add("trim mustache");
+        poirotsToDos.add("call all the suspects together");
+        poirotsToDos.add("reveal the truth of the crime");
+
+        //printing out the size of each array list
+        System.out.println("Sherlock has " + sherlocksToDos.size() + " to dos.");
+        System.out.println("Poirot has " + poirotsToDos.size() + " to dos.");
+
+        //the detective with the most to-dos
+        System.out.println("Poirot has the most todos.");
+
     }
 }
