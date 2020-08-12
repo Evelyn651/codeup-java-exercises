a1fd491cd33e844bdabcb8cd2c8a35b87ee28382
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-08-12 14:02:01 -0500

    feat: working with while loops and incrementing

---
+++ src/CodecademyJavaPractice/Coffee.java
@@ -0,0 +1,13 @@
+package CodecademyJavaPractice;
+
+public class Coffee {
+    public static void main(String[] args){
+        //initialize cupsOfCoffee loop
+        int cupsOfCoffee = 1;
+        //while loop with counter
+        while(cupsOfCoffee <= 100){
+            System.out.println("Fry drinks cup of coffee #" + cupsOfCoffee);
+            cupsOfCoffee++;
+        }
+    }
+}
