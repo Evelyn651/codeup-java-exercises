788273c316a3cfb8fe2742a0b4dfe4f5e0041045
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-08-11 22:32:48 -0500

    feat: using a while loop to generate a die roll until it landed on 5, reset the die roll count with the random generator

---
+++ src/CodecademyJavaPractice/LuckyFive.java
@@ -0,0 +1,21 @@
+package CodecademyJavaPractice;
+import java.util.Random;
+
+public class LuckyFive {
+    public static void main(String[] args) {
+
+        //A random number generator
+        Random randomGenerator = new Random();
+
+        //generates a number between 1 and 6
+        int dieRoll = randomGenerator.nextInt(6) + 1;
+
+        //repeat while role isn't 5
+        while (dieRoll != 5) {
+            System.out.println(dieRoll);
+
+            //resets die once it "lands" on 5
+            dieRoll = randomGenerator.nextInt(6) + 1;
+        }
+    }
+}
