d2782a874a09922a52dd67c79ccc7f952d956a10
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-08-13 16:10:07 -0500

    feat: practice with charAt()

---
+++ src/CodecademyJavaPractice/Initials.java
@@ -0,0 +1,15 @@
+package CodecademyJavaPractice;
+
+public class Initials {
+    public static void main(String[] args){
+        String firstName = "Bilbo";
+        String lastName = "Baggins";
+
+        //get initials
+        System.out.println(firstName.charAt(0) + "." + lastName.charAt(0));
+
+        String lotr = "The Lord of the Rings";
+
+        System.out.println(lotr.charAt(8)); //spaces are counted in indexes so 3, 8, 11, 15 print nothing
+    }
+}
