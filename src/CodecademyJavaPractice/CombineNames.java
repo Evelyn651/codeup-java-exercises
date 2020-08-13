40b8fa40cb25125a9c27ab5e6a98f4d520b31f62
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-08-13 12:29:22 -0500

    feat: learning how to use concat()

---
+++ src/CodecademyJavaPractice/CombineNames.java
@@ -0,0 +1,20 @@
+package CodecademyJavaPractice;
+
+public class CombineNames {
+    public static void main(String[] args){
+        //combining first name and last name
+        String firstName = "Evelyn";
+        String lastName = "Acosta";
+
+        System.out.println(firstName.concat(" " + lastName));
+
+        //combining the alphabet
+        String letters = "abcdefg";
+        String moreLetters = "hijklmnop";
+        String evenMoreLetters = "qrstuvwxyz";
+
+        System.out.println("The alphabet: " + letters.concat(moreLetters + evenMoreLetters));
+
+
+    }
+}
