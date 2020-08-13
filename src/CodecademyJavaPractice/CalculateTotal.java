7c907f727af56152897050dbda7fd65010a7e5d2
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-08-12 22:14:21 -0500

    feat: working with an enhanced for loop to go through each index in arraylist to get the value, the other for loop is to add the values together for the total

--- src/CodecademyJavaPractice/CalculateTotal.java
+++ src/CodecademyJavaPractice/CalculateTotal.java
@@ -12,11 +12,20 @@ public class CalculateTotal {
         expenses.add(81.37);
 
         double total = 0;
+        double mostExpensive = 0;
 
-        //to iterate over the arraylist
+        //to iterate over the arraylist for total
         for(int i = 0; i < expenses.size(); i++){
             total += expenses.get(i);
         }
         System.out.println("Total expenses: $" + total);
+
+        //using enhanced for loop to go through each index of arraylist
+        for(double expense : expenses){
+            if(expense > mostExpensive){
+                mostExpensive = expense;
+            }
+        }
+        System.out.println(mostExpensive);
     }
 }
