5feac704ec8e48eac6428c26e359b1ec8b0eeaad
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-08-13 11:33:52 -0500

    feat: practice with string methods length(), substring(), and toUpperCase()

---
+++ src/CodecademyJavaPractice/CodecademyHelloWorld.java
@@ -0,0 +1,12 @@
+package CodecademyJavaPractice;
+
+public class CodecademyHelloWorld {
+    public static void main(String[] args){
+        String str = "Hello, World!";
+
+        //practice with string methods
+        System.out.println("length: " + str.length());
+        System.out.println("substring: " + str.substring(4));
+        System.out.println("capitalizing: " + str.toUpperCase());
+    }
+}
