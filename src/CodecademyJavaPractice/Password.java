fcb7a3fee6fd54fb802b2e11c0e1870308394c72
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-08-13 15:06:56 -0500

    feat: practice with equals()

---
+++ src/CodecademyJavaPractice/Password.java
@@ -0,0 +1,9 @@
+package CodecademyJavaPractice;
+
+public class Password {
+    public static void main(String[] args) {
+    String password = "supersecretpassword";
+    //to see if password matches //returns boolean value whether it matches(true) or not (false)
+        System.out.println(password.equals("loop"));
+    }
+}
