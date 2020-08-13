cb72f71ee8475b2b705c7b3b445bb6be296475b9
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-08-12 21:47:19 -0500

    feat: refactored while loop into a for loop for cupsOfCoffee exercise

--- src/CodecademyJavaPractice/Coffee.java
+++ src/CodecademyJavaPractice/Coffee.java
@@ -3,11 +3,15 @@ package CodecademyJavaPractice;
 public class Coffee {
     public static void main(String[] args){
         //initialize cupsOfCoffee loop
-        int cupsOfCoffee = 1;
+//        int cupsOfCoffee = 1;
         //while loop with counter
-        while(cupsOfCoffee <= 100){
-            System.out.println("Fry drinks cup of coffee #" + cupsOfCoffee);
-            cupsOfCoffee++;
+//        while(cupsOfCoffee <= 100){
+//            System.out.println("Fry drinks cup of coffee #" + cupsOfCoffee);
+//            cupsOfCoffee++;
+//        }
+        //refactor into a for loop
+        for(int cupsOfCoffee = 1; cupsOfCoffee <= 100; cupsOfCoffee++){
+            System.out.println("For Loop for - Fry drinks cup of coffee #" + cupsOfCoffee);
         }
     }
 }
