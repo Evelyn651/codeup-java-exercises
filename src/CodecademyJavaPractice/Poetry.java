a7ee475546f95a23b2444bae2456097fdbcc494e
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-08-13 16:21:08 -0500

    feat: practice with substring() and how to count the chars

---
+++ src/CodecademyJavaPractice/Poetry.java
@@ -0,0 +1,15 @@
+package CodecademyJavaPractice;
+
+public class Poetry {
+    public static void main(String[] args){
+        String line = "The Heav'ns and all the Constellations rung";
+        //looking for each word
+        System.out.println(line.substring(0, 3));
+        System.out.println(line.substring(4, 11));
+        System.out.println(line.substring(12, 15));
+        System.out.println(line.substring(16, 19));
+        System.out.println(line.substring(20, 23));
+        System.out.println(line.substring(24, 38));
+        System.out.println(line.substring(39, 43));
+    }
+}
