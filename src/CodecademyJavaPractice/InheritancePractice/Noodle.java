28aa4f688b0fa432b808c240fee49f554b478dc6
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-08-15 00:01:07 -0500

    feat: working with consrtuctors and super()

--- src/CodecademyJavaPractice/InheritancePractice/Noodle.java
+++ src/CodecademyJavaPractice/InheritancePractice/Noodle.java
@@ -2,8 +2,17 @@ package CodecademyJavaPractice.InheritancePractice;
 
 public class Noodle {
     double lengthInCentimeters;
+    double widthInCentimeters;
     String shape;
+    String ingredients;
     String texture = "brittle";
+
+    Noodle(double lenInCent, double wthInCent, String shp, String ingr){
+        this.lengthInCentimeters = lenInCent;
+        this.widthInCentimeters = wthInCent;
+        this.shape = shp;
+        this.ingredients = ingr;
+    }
 
     public void cook(){
         this.texture = "cooked";
@@ -12,6 +21,9 @@ public class Noodle {
     public static void main(String[] args){
         Spaghetti spaghettiPomodoro = new Spaghetti();
         System.out.println(spaghettiPomodoro.texture);
+
+        Pho phoChay = new Pho();
+        System.out.println(phoChay.shape);
     }
 
 }
--- src/CodecademyJavaPractice/InheritancePractice/Spaghetti.java
+++ src/CodecademyJavaPractice/InheritancePractice/Spaghetti.java
@@ -2,4 +2,7 @@ package CodecademyJavaPractice.InheritancePractice;
 
 public class Spaghetti extends Noodle{
 
+    Spaghetti(double lenInCent, double wthInCent, String shp, String ingr) {
+        super(lenInCent, wthInCent, shp, ingr);
+    }
 }
