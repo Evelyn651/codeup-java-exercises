ffebfde971c1858e4b92fcab79f4867d24426bd4
Evelyn Acosta <acostaevelyn7911@gmail.com>
2020-08-19 13:48:13 -0500

    feat: debugging a run-time error.

--- src/CodecademyJavaPractice/Bugs/Debug.java
+++ src/CodecademyJavaPractice/Bugs/Debug.java
@@ -2,9 +2,23 @@ package CodecademyJavaPractice.Bugs;
 
 public class Debug {
     public static void main(String[] args){
+
+        //syntax error
         System.out.println("       1");
         System.out.println("     2 3");
         System.out.println("   4 5 6");
         System.out.println("7 8 9 10");
+        // last sout was missing ;
+
+        //run-time error
+        int width = 20;
+        int length = 40;
+
+        int ratio = length / width;
+        System.out.println(ratio);
+        //missing 2 in 20
+
+
+
     }
 }
